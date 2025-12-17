fe32eb7a772b4a520beb6c6c41cf8929facfc847
chafisi <62420985+chafisi@users.noreply.github.com>
2025-12-17 10:39:34 +0100

    poner un log para ver si carga los eventos

--- javascript2.js
+++ javascript2.js
@@ -114,6 +114,7 @@ let GAS_WEB_APP_URL = 'TU_URL_DE_GOOGLE_APPS_SCRIPT_AQUI';
 
 // Carga inicial de datos desde la API
 async function cargarEventos() {
+    console.log("Iniciando cargarTarjetas..."); // LOG 1
     const spinner = document.getElementById('loading-spinner');
     spinner.classList.remove('hidden');
     document.getElementById('events-list').innerHTML = ''; // Limpia por si acaso
